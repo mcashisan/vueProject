8eac0a06536f8993458117482541d3f9c7a53d9f
hello <771173220@qqcom>
2019-10-12 08:58:53 +0800

    2019/10/12再次更新

--- ele/src/router/index.js
+++ ele/src/router/index.js
@@ -3,18 +3,18 @@ import Router from 'vue-router'
 //引入组件配置路由
 import Home from "../components/Home/Home"
 import Login from "../components/Login"
-<<<<<<< Updated upstream
+//
 import Reset from "../components/Lyf/ResetPassword"
 import Address from "../components/Lyf/Address"
 import AddAddress from "../components/Lyf/AddAddress"
-=======
+//
+
 //城市搜索: city
 import City from "../components/Home/City"
 //商品展示页面: msite
 import Msite from "../components/Home/Msite"
 // 商品列表
 import ShopList from "../components/ShopList"
->>>>>>> Stashed changes
 
 Vue.use(Router)
 
@@ -23,17 +23,14 @@ export default new Router({
     {path: "/", redirect: "/home"},
     {path:"/home", component:Home},
     {path: "/login", component: Login},
-<<<<<<< Updated upstream
     {path:"/reset",component:Reset},
     {path:"/address",component:Address,
-      children:[{path:"address1",component:AddAddress}]}
-=======
+      children:[{path:"address1",component:AddAddress}]},
     // 城市搜索
     {path: "/city", component: City},
     //商品展示页面
     {path:"/msite", component: Msite},
     //
     {path:"/shoplist", component: ShopList}
->>>>>>> Stashed changes
   ]
 })
